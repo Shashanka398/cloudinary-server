55b9bd4741c759e4c61f205a01ac63fae92ad979
Shashank <shashanksagar398@gmail.com>
2024-12-13 20:36:23 +0530

    revert app.js

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const fileUpload = require("express-fileupload");
-const {uploadDetails,getUserDetails} =  require("./controller/userController")
-const router = express.Router();
 dotenv.config();
 
-// Middleware
+
 app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: '/tmp/'
@@ -18,21 +16,23 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-// Import routes
+
 const userRoutes = require('./route/route');
 
-// Routes
-console.log("Entered backend");
+
 app.use("/api/v1", userRoutes);
 
-// Test route (this can be accessed at /api/)
-app.get("/",getUserDetails );
-router.post("/upload",uploadDetails)
+app.get("/",(req,res)=>{
+    res.json({
+        success:true,
+        message:"Backend running in vercel !!!!"
+    })
+} );
 
-// Connect to Cloudinary and database
+
 const cloudinary = require("./config/cloudinary");
 cloudinary.cloudinaryConnect();
 database.connect();
 
-// Export the Express app as a Vercel function
+
 module.exports = app;
