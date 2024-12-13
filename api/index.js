dd637dbf13a5a85433b85eb99037b57089d311b0
Shashank <shashanksagar398@gmail.com>
2024-12-13 19:14:09 +0530

    configure

--- api/index.js
+++ api/index.js
@@ -5,23 +5,25 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const fileUpload = require("express-fileupload");
-dotenv.config(); 
-const PORT = process.env.PORT || 5000; 
+dotenv.config();
+
+// Middleware
 app.use(fileUpload({
-    useTempFiles : true,
-    tempFileDir : '/tmp/'
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
 }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-
+// Import routes
 const userRoutes = require('../route/route');
 
-console.log("Entered backend")
+// Routes
+console.log("Entered backend");
 app.use("/api/v1", userRoutes);
 
-// Test route
+// Test route (this can be accessed at /api/)
 app.get("/", (req, res) => {
   return res.json({
     success: true,
@@ -29,9 +31,10 @@ app.get("/", (req, res) => {
   });
 });
 
+// Connect to Cloudinary and database
 const cloudinary = require("../config/cloudinary");
 cloudinary.cloudinaryConnect();
 database.connect();
-app.listen(PORT, () => {
-  console.log(`App is running at ${PORT}`);
-});
+
+// Export the Express app as a Vercel function
+module.exports = app;
